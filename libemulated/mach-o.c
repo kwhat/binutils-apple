c92593b0be6fe59a9c007a7082856a2f7122b182
Alexander Barker <alex@1stleg.com>
2019-07-24 14:52:22 -0700

    Fixing prefix issue

--- libemulated/mach-o.c
+++ libemulated/mach-o.c
@@ -1,14 +1,15 @@
 #include <unistd.h>
-
+#include <string.h>
 int _NSGetExecutablePath(char *path, unsigned int *size)
 {
    int bufsize = *size;
    int ret_size;
-   ret_size = readlink("/proc/self/exe", path, bufsize);
+   char *localpath = (char*)malloc(bufsize);
+   ret_size = readlink("/proc/self/exe", localpath, bufsize);
    if (ret_size != -1)
    {
         *size = ret_size;
-        path[ret_size]=0;
+        strcpy(path,localpath);
         return 0;
    }
    else
